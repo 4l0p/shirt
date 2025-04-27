e06ac43223353b56fb2bbdbac5daedd74ec5d650
André L de O Pereira <alop.mail@gmail.com>
2025-04-27 11:54:48 -0300

    80 - implementacao button float mobile

--- js/float.js
+++ js/float.js
@@ -1,5 +1,5 @@
 const elementoAlvo = document.querySelector('.float-desk');
-// const elementoAlvo = document.getElementById('elemento-que-aparece');
+const elementoAlvoMobile = document.querySelector('.float-mobile__button');
 const elementoMarcador = document.getElementById('elemento-referencia');
 
 const observer = new IntersectionObserver((entries) => {
@@ -7,10 +7,12 @@ const observer = new IntersectionObserver((entries) => {
     // Quando o marcador começa a sair da viewport pelo topo (rolagem para baixo)
     if (entry.boundingClientRect.top < 0) {
       elementoAlvo.style.display = 'flex';
+      elementoAlvoMobile.style.display = 'block'
     } 
     // Quando o marcador está totalmente visível novamente (rolagem para cima)
     else if (entry.isIntersecting && entry.boundingClientRect.top >= 0) {
       elementoAlvo.style.display = 'none';
+      elementoAlvoMobile.style.display = 'none';
     }
   });
 }, {
